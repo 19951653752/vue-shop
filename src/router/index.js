c859400e6806a0e9dc1b7bc1b074aec66212eb32
liao gang <2216934634@qq.com>
2020-08-25 17:51:29 +0800

    完成了登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: () => import('../components/Login') },
+  { path: '/home', component: () => import('../components/Home') }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to,from,next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转过来
+  // next 是一个函数，表示放行
+
+  // 如果用户要去登录页，则放行
+  if(to.path === '/login') return next()
+  // 如果用户没有登录，则强制跳转到登录页
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  // 用户登录，直接放行
+  next()
 })
 
 export default router
